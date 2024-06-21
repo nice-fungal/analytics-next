aecbb36f5aa86367ace3a042397b5b43c2bc4955
hbrls <shuaizhexu@gmail.com>
2024-06-21 13:57:51 +0800

    !: RFC: 使用 loadSettings

--- packages/browser/src/browser/standalone-interface.ts
+++ packages/browser/src/browser/standalone-interface.ts
@@ -1,10 +1,11 @@
-import { Analytics, InitOptions } from '../core/analytics'
+import { Analytics, AnalyticsSettings, InitOptions } from '../core/analytics'
 
 export interface AnalyticsSnippet extends AnalyticsStandalone {
   load: (writeKey: string, options?: InitOptions) => void
 }
 
 export interface AnalyticsStandalone extends Analytics {
+  _loadSettings?: AnalyticsSettings
   _loadOptions?: InitOptions
   _writeKey?: string
   _cdn?: string
